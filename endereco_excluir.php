049ee8e5cbc6b410a097451f6d367a1a3a86bb38
Adriel Kasima <adrielkasima@gmail.com>
2023-05-31 17:01:05 -0300

    excluir endereço + verif de pedidos

--- endereco_excluir.php
+++ endereco_excluir.php
@@ -3,28 +3,43 @@
 require __DIR__.'/vendor/autoload.php';
 
 use App\Entity\Endereco;
+use App\Entity\Pedido;
+use App\Entity\Item_Pedido;
 use App\Session\Login;
 
-  $obEndereco = Endereco::getEndereco($_GET['codend']);
-  $usuarioLogado = Login::getUsuarioLogado();
+$alerta = "";
 
-if(!isset($_GET['codend']) || !is_numeric($_GET['codend']) || $usuarioLogado['codus'] != $obEndereco->codus) {
+$obEndereco = Endereco::getEndereco($_GET['codend']);
+$usuarioLogado = Login::getUsuarioLogado();
+$obPedidos = Pedido::getPedidosCli($usuarioLogado['codus']);
+
+if (!isset($_GET['codend']) || !is_numeric($_GET['codend']) || $usuarioLogado['codus'] != $obEndereco->codus) {
     header('Location: index.php?status=error');
     exit;
 }
 
-$obEndereco = Endereco::getEndereco($_GET['codend']);
-
-if(!$obEndereco instanceof Endereco) {
-    header ('location: index.php?status=error');
+if (!$obEndereco instanceof Endereco) {
+    header('Location: index.php?status=error');
     exit;
 }
 
-if(isset($_POST['excluir'])){
-    $obEndereco->excluir();
-    header('Location: dados_listar.php?codus=' . $usuarioLogado['codus']);
-    exit;
-}
+if (isset($_POST['acao']) && $_POST['acao'] == 'excluir') {
+    $temPedidoAberto = false;
 
+    foreach ($obPedidos as $obPedido) {
+        if ($obPedido->codend == $_GET['codend'] && in_array($obPedido->status_pedido, ["Pago", "Em aberto", "Em preparação", "Entregue a transportadora"])) {
+            $temPedidoAberto = true;
+            break;
+        }
+    }
+
+    if ($temPedidoAberto) {
+        $alerta = "Não é possível excluir o endereço, pois existem pedidos associados a ele.";
+    } else {
+        $obEndereco->excluir();
+        header('Location: dados_listar.php?codus=' . $usuarioLogado['codus']);
+        exit;
+    }
+}
 
 include __DIR__.'/public/includes/endereco_conf.php';
--- public/includes/endereco_conf.php
+++ public/includes/endereco_conf.php
@@ -1,3 +1,6 @@
+<?php
+  $alerta = strlen($alerta) ? '<div>'.$alerta.'</div>' : '';
+?>
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,67 +11,42 @@
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" />
     <link rel="icon" type="imagem/png" href="./public/img/logopetfarma.png" />
     <link rel="stylesheet" href="./public/css/StyleExcluirEndereco.css">
-  </head>
+</head>
 
-  <body>
+<body>
     <main>
-<section id="header">
-      <a href="index.php"><img src="./public/img/logopetfarma2.png" class="logo" alt=""></a>
+      <section id="header">
+        <a href="index.php"><img src="./public/img/logopetfarma2.png" class="logo" alt=""></a>
 
-      <div>
-        <ul id="navbar">
-          <li><i class="fas fa-solid fa-lock"></i>
-            <h3>Ambiente seguro</h3>
-          </li>
-        </ul>
-      </div>
-    </section>
+        <div>
+          <ul id="navbar">
+            <li><i class="fas fa-solid fa-lock"></i>
+              <h3>Ambiente seguro</h3>
+            </li>
+          </ul>
+        </div>
+      </section>
     
-    <form method="post">
-      
-    <div class="form-group">
-      <h2>Excluir Endereço</h2>
-      <p>Você deseja realmente excluir o endereço <strong><?=$obEndereco->rua.', '. $obEndereco->numero.' - '. $obEndereco->bairro ?></strong>?</p>
-    <!-- </div>
+      <form method="post">
+        
+        <div class="form-group">
+          <h2>Excluir Endereço</h2>
+          <p>Você deseja realmente excluir o endereço <strong><?=$obEndereco->rua.', '. $obEndereco->numero.' - '. $obEndereco->bairro ?></strong>?</p>
+        <!-- </div>
 
-    <div class="form-group"> -->
-      
-      <div class="btnContainer">
-        <a href="dados_listar.php?codus=<?=$usuarioLogado['codus']?>">
-          <button class="btnExcluir" type="button">Cancelar</button>
-          <button class="btnExcluir" type="submit" name="excluir">Excluir</button>
-        </a>
-      </div>
+        <div class="form-group"> -->
+          
+          <div class="btnContainer">
+            <a href="dados_listar.php?codus=<?=$usuarioLogado['codus']?>">
+              <button class="btnExcluir" type="button">Cancelar</button>
+              <button class="btnExcluir" type="submit" name="acao" value="excluir">Excluir</button>
+            </a>
+            <?=$alerta?>
+          </div>
 
-    </div>
+        </div>
 
-  </form>
-
-</main>
-  </body>
+      </form>
+    </main>
+</body>
   
-
-<!-- <main>
-
-  <h2>Excluir Endereço</h2>
-
-  <form method="post">
-
-    <div class="form-group">
-      <p>Você deseja realmente excluir o endereço <strong><?=$obEndereco->rua.', '. $obEndereco->numero.' - '. $obEndereco->bairro ?></strong>?</p>
-    </div>
-
-    <div class="form-group">
-      
-      <div class="btnContainer">
-        <a href="dados_listar.php?codus=<?=$usuarioLogado['codus']?>">
-        <button type="button">Cancelar</button>
-      </a>
-  <button class="btnExcluir" type="submit" name="excluir">Excluir</button>
-</div>
-
-    </div>
-
-  </form>
-
-</main> -->
