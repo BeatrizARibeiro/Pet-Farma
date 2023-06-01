98236abf801be46813df810cae6fc67c20ec1c05
Yasmin <yasmin.rosas@lab2dev.com>
2023-06-01 16:22:14 -0300

    Tela de cacelar pedido

--- public/includes/pedido_cancelar_conf.php
+++ public/includes/pedido_cancelar_conf.php
@@ -1,21 +1,36 @@
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Excluir Produto</title>
+
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" />
+    <link rel="icon" type="imagem/png" href="./public/img/logopetfarma.png" />
+    <link rel="stylesheet" href="./public/css/StyleExcluirEndereco.css">
+  </head>
+
+  <body>
 <main>
 
-  <h2>Cancelar Pedido</h2>
-
+  
   <form method="post">
-
+    
     <div class="form-group">
+      <h2>Cancelar Pedido</h2>
       <p>Você deseja realmente cancelar o pedido nº <strong><?=$pedido->numpedido?></strong>? &#128542;</p>
-    </div>
+    <!-- 
 
-    <div class="form-group">
+    <div class="form-group"> -->
+    <div class="btnContainer">
       <a href="pedido_ver.php?numpedido=<?=$pedido->numpedido?>">
-        <button type="button">Não</button>
+        <button class="btnExcluir" type="button">Não</button>
+        <button class="btnExcluir" type="submit" name="sim">Sim</button>
       </a>
-
-      <button type="submit" name="sim">Sim</button>
     </div>
-
+</div>
   </form>
 
 </main>
+
+</body>
