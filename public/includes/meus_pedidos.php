03a64bf78a1bec592237b899a8ae1d09a3590166
Yasmin <yasmin.rosas@lab2dev.com>
2023-06-06 14:18:08 -0300

    botao e form alinhado

--- public/includes/meus_pedidos.php
+++ public/includes/meus_pedidos.php
@@ -37,7 +37,7 @@
                             <td>'.$pedido->status_pedido.'</td>
                             <td>'.implode('/', array_reverse(explode('-', $pedido->dt_pedido))).'</td>
                             <td>R$ '.number_format($valorTotal, 2, ',', '.').'</td>
-                            <td><a href="pedido_ver.php?numpedido='.$pedido->numpedido.'">Ver</a></td>
+                            <td><a class="btnVer" href="pedido_ver.php?numpedido='.$pedido->numpedido.'">Ver</a></td>
                     </tr>';
     }
 
