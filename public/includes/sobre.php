0feec364ce0549b09e57989f3e8d696569be3286
Yasmin <yasmin.rosas@lab2dev.com>
2023-06-09 16:50:46 -0300

    tela sobre sem responsividade

--- public/includes/sobre.php
+++ public/includes/sobre.php
@@ -1,15 +1,41 @@
+<head>
+<meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link
+    rel="stylesheet"
+    href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+  />
+  <link rel="icon" type="imagem/png" href="./public/img/logopetfarma.png" />
+  <link rel="stylesheet" href="./public/css/StyleSobre.css" />
+
+</head>
+
 <body>
-    <main>
-        <a href="index.php">
-            <button>Voltar</button>
-        </a>
-        
-        <h1>Quem somos</h1>
-        <h3>Bem-vindo(a) à Pet-Farma - Sua Farmácia para Animais de Estimação!</h3>
-        <p>Na Pet-Farma, temos o prazer de oferecer a você e seus amados animais de estimação uma experiência única em cuidados farmacêuticos. Somos uma farmácia especializada em fornecer produtos de alta qualidade, exclusivamente para animais de estimação.</p>
+    <!-- <main> -->
+        <div class="container">
+            <div >
+                <a href="index.php">
+                    <button class="form-button">Voltar</button>
+                </a>
+            </div>
+            <div class="sobre">
+                <div class="titulo">
+                    <h1>Quem somos</h1>
+                    <h3>Bem-vindo(a) à Pet-Farma - Sua Farmácia para Animais de Estimação!</h3>
+                </div>
+                <div class="alinhamento">
+                    <img class="img" src="./public/img/cat.png">
 
-        <p>Sabemos o quanto os animais de estimação são importantes para as famílias. Eles não são apenas companheiros leais, mas também membros queridos que merecem o melhor atendimento possível. É por isso que a Pet-Farma foi fundada com um compromisso apaixonado de cuidar da saúde e do bem-estar dos nossos bichinhos.</p>
+                <div class="texto">
+                    <p>Na Pet-Farma, temos o prazer de oferecer a você e seus amados animais de estimação uma experiência única em cuidados farmacêuticos. Somos uma farmácia especializada em fornecer produtos de alta qualidade, exclusivamente para animais de estimação.</p>
+                    <p>Sabemos o quanto os animais de estimação são importantes para as famílias. Eles não são apenas companheiros leais, mas também membros queridos que merecem o melhor atendimento possível. É por isso que a Pet-Farma foi fundada com um compromisso apaixonado de cuidar da saúde e do bem-estar dos nossos bichinhos.</p>
+                    <p>Além dos medicamentos, também fornecemos uma variedade de produtos de saúde, como suplementos, vitaminas, produtos para higiene, acessórios e muito mais. Nossa seleção é cuidadosamente escolhida para atender às necessidades individuais de cada animal e raça.</p>
+                </div>
+                </div>
+            </div>
+        </div>
 
-        <p>Além dos medicamentos, também fornecemos uma variedade de produtos de saúde, como suplementos, vitaminas, produtos para higiene, acessórios e muito mais. Nossa seleção é cuidadosamente escolhida para atender às necessidades individuais de cada animal e raça.</p>
-    </main>
+
+    <!-- </main> -->
 </body>
